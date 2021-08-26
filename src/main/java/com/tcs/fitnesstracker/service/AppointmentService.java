1145201c513df90964980f1929e2c70fcad796ca
TcsSurya <tcssurya51@gmail.com>
2021-08-26 17:53:17 +0530

    sonarqube Mapping Transaction Lombok Quiz added

--- src/main/java/com/tcs/fitnesstracker/service/AppointmentService.java
+++ src/main/java/com/tcs/fitnesstracker/service/AppointmentService.java
@@ -3,10 +3,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tcs.fitnesstracker.Appointment;
-import com.tcs.fitnesstracker.AppointmentNotFoundException;
+import com.tcs.fitnesstracker.User;
 import com.tcs.fitnesstracker.repository.IAppointmentRepository;
+import com.tcs.fitnesstracker.exception.*;
+
+import org.springframework.util.StringUtils;
 
 
 
@@ -17,10 +21,22 @@ public class AppointmentService implements IAppointmentService {
 IAppointmentRepository appointmentRepo;
 
 	@Override
-	
+	@Transactional
 	public void save(Appointment appointment) {
+		Appointment a1=new Appointment();
+		a1.setAmount(appointment.getAmount());
+		a1.setAppointmentId(appointment.getAppointmentId());
+		a1.setPackageName(appointment.getPackageName());
+		a1.setPhysioRequired(appointment.isPhysioRequired());
+		a1.setTrainerName(appointment.getTrainerName());
+//		User u1=new User();
+//		u1.setFirstName("Test User");
+//		a1.setUser(u1);
+        a1.setUser(appointment.getUser());
+		
 		// TODO Auto-generated method stub
-		appointmentRepo.save(appointment);
+		//System.out.println(appointment.getUser().getFirstName());
+		appointmentRepo.save(a1);
 		System.out.println("Appointment saved");
 		
 		}
@@ -50,6 +66,32 @@ IAppointmentRepository appointmentRepo;
 		}
 		appointmentRepo.deleteById(id);
 
+	}
+	@Override
+	public void updateAppointment(Appointment appointment, Integer id) {
+		Optional<Appointment> appointmentFromDB = appointmentRepo.findById(id);
+		if (!appointmentFromDB.isPresent()) {
+			throw new AppointmentNotFoundException("appointment  does not exist");
+		}
+		Appointment appointment1 = appointmentFromDB.get();
+		if (StringUtils.hasText(appointment.getTrainerName())) {
+			appointment1.setTrainerName(appointment.getTrainerName());
+		}
+		if(StringUtils.hasText(appointment.getPackageName())) {
+			appointment1.setPackageName(appointment.getPackageName());
+		}
+		if(appointment.getAmount()>0)
+			appointment1.setAmount(appointment.getAmount());
+		if(appointment.isPhysioRequired())
+			appointment1.setPhysioRequired(true);
+		else if(appointment1.isPhysioRequired()==true && appointment.isPhysioRequired()==false)
+			appointment1.setPhysioRequired(false);
+
+		
+		
+		appointmentRepo.save(appointment1);
+
+	
 	}
 
 }
